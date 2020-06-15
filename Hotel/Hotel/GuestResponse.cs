a78937618b991eebee2274c9255017994d9cef07
User <User@WIN-PAI2S3FL083>
2020-06-15 13:09:04 +0300

    Add classes v1

---
+++ Hotel/Hotel/GuestResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Hotel
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Fname { get; set; }
+        public string Marck_avto { get; set; }
+        public string Number_avto { get; set; }
+
+    }
+}
